bf4a821ee76456690c3a71c37a25d7f92b9c5c9f
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-09-05 21:01:13 +0300

    feat: done Docker

--- car_api/tests.py
+++ car_api/tests.py
@@ -1,23 +1,21 @@
-from _typeshed import Self
-from django import test
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Car
 
 # Create your tests here.
-class BlogTests(TestCase):
+class CarsTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         testuser1 = get_user_model().objects.create_user(username='testuser1', password='pass')
         testuser1.save()
-        test_post = Car.objects.create(title='Test Post Body', author=testuser1)
+        test_post = Car.objects.create(author=testuser1, modelCar='Test Post Body', description='Test Post Body' )
         test_post.save()
 
     def test_blog_content(self):
         post = Car.objects.get(id = 1)
-        expected_auther = f'{post.auther}'
-        expected_title = f'{post.title}'
-        expected_body = f'{post.body}'
+        expected_auther = f'{post.author}'
+        expected_modelCar = f'{post.modelCar}'
+        expected_description = f'{post.description}'
         self.assertEqual(expected_auther, 'testuser1')
-        self.assertEqual(expected_title, 'Test Post')
-        self.assertEqual(expected_body, 'Test Post Body')
+        self.assertEqual(expected_modelCar, 'Test Post Body')
+        self.assertEqual(expected_description, 'Test Post Body')
